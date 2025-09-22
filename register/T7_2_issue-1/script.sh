cee679acbb8dca6e299332921fb5487001ec901c
Jose Rafael Ferrer-Paris <jr.ferrer.paris@gmail.com>
2025-09-22 21:59:13 +1000

    Draft of the public register

---
+++ register/T7_2_issue-1/script.sh
@@ -0,0 +1,3 @@
+
+
+pandoc -t markdown --extract-media=./media/ T7-2_consultationdraft_GET-SC_20250909.docx -o index.qmd
---
+++ register/T7_3_issue-1/script.sh
@@ -0,0 +1,2 @@
+
+pandoc -t markdown --extract-media=./media/ T7-3_consultationdraft_GET-SC_20250909.docx -o index.qmd
